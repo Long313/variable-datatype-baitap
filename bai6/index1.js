2d09b9c116930420590bcaeb42f4782792939de4
Long <tranxuanlonga555@gmail.com>
2022-03-14 16:44:08 +0700

    first commit Signed-off-by: Long <tranxuanlonga555@gmail.com>

---
+++ bai6/index1.js
@@ -0,0 +1,9 @@
+let soTienVay = prompt("Số tiền vay ngân hàng là:");
+let so_tien_vay = parseInt(soTienVay);
+let soNam = prompt("Số năm vay tiền:");
+let so_nam = parseInt(soNam);
+let tienLai = so_tien_vay*6.9/100;
+let tien_lai = parseInt(tienLai);
+let soTienTra = so_tien_vay + tien_lai;
+let so_tien_tra = parseInt(soTienTra);
+document.write(so_tien_tra);
